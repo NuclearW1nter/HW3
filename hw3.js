7c9dfcead59dc2ff35805d68ca47514fd9a55530
Justin Plassmeyer <jplassme@masonlive.gmu.edu>
2017-09-26 05:57:53 -0400

    final commit

--- hw3.js
+++ hw3.js
@@ -171,6 +171,7 @@ var promise;
 var difficultChamps = new Map();
 var championName = ' ';
 var champRawData;
+app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
